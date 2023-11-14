396a4181608355296a8a4cf268c8f97b9eb31ad2
bakhaissakhov <bakha87@gmail.com>
2023-11-14 14:35:37 -0500

    added ec

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+  }
+
+  # backend "azurerm" {
+  #   resource_group_name = "rg-sp-terraform"
+  #   storage_account_name = "mytfstatetest"
+  #   container_name = "my-container"
+  #   key = "tfstate-dev"
+  # }
+
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+
+resource "azurerm_elastic_cloud_elasticsearch" "cloud_elasticsearch" {
+  # (Other properties like name, location, resource group name, etc.)
+  name                        = "test-es-xxx"
+  resource_group_name         = var.rg_name
+  location                    = "westus2"
+  sku_name                    = "ess-consumption-2024_Monthly@TIDgmz7xq9ge3py"
+  elastic_cloud_email_address = "example@gmail.com"
+  # monitoring_enabled = false
+  tags = {
+    "Environment" = "dev"
+    "Project"     = "example"
+    # Add other tags as needed
+  }
+}
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "tenant_id" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "client_id" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
+variable "application_name" {
+  type        = list(string)
+  description = "(optional) describe your variable"
+  default     = ["myapp1", "myapp2"]
+}
+
+variable "environment_name" {
+  type        = list(string)
+  description = "(optional) describe your variable"
+  default     = ["dev", "qa", "prod"]
+}
+
+variable "location" {
+  type        = string
+  description = "(optional) describe your variable"
+  default     = "centralus"
+}
+
+variable "rg_name" {
+  type        = string
+  description = "(optional) describe your variable"
+  default     = "rg-sp-terraform"
+}
